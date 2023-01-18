9d1b67fd61e9f26ffb68daa0c1b18507da52b23c
emanikor <87859876+emanikor@users.noreply.github.com>
2023-01-18 15:28:50 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const menuBtn = document.querySelector('#menu-btn');
-const navBar = document.querySelector('.navbar');
- 
-menuBtn.addEventListener('click', () =>{
-    navBar.classList.toggle('active')
-})
+// const menuBtn = document.querySelector('#menu-btn');
+let navBar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navBar.classList.toggle('active');
+    cartItem.classList.remove('active');
+    searchBtn.classList.remove('active')
+}
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navBar.classList.remove('active');
+    searchBtn.classList.remove('active')
+}
+let searchBtn = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchBtn.classList.toggle('active');
+    navBar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+window.onscroll =()=>{
+    navBar.classList.remove('active');
+    cartItem.classList.remove('active');
+    searchBtn.classList.remove('active');
+}
