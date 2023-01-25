c50684cc82d897c01c082b19c45e31428787eb8e
emanikor <87859876+emanikor@users.noreply.github.com>
2023-01-25 15:03:48 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -26,3 +26,19 @@ window.onscroll =()=>{
     cartItem.classList.remove('active');
     searchBtn.classList.remove('active');
 }
+// Initialize Swiper 
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+   loop:true,
+  });
+
+
